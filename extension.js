f15375dd89b71ce347df5aa93e425462f0013a7b
Even <mugi628@gmail.com>
2019-07-05 12:14:58 +0800

    update opencc version

--- extension.js
+++ extension.js
@@ -1,62 +1,46 @@
-'use strict';
-
-const
-  opencc = require('node-opencc'),
-  vscode = require('vscode');
+const opencc = require('node-opencc'),
+	vscode = require('vscode');
 
 function activate(context) {
-  [
-    'hongKongToSimplified',
-    'simplifiedToHongKong',
-    'simplifiedToTraditional',
-    'simplifiedToTaiwan',
-    'simplifiedToTaiwanWithPhrases',
-    'traditionalToHongKong',
-    'traditionalToSimplified',
-    'traditionalToTaiwan',
-    'taiwanToSimplified',
-    'taiwanToSimplifiedWithPhrases'
-  ].map(name => {
-    context.subscriptions.push(
-      vscode.commands.registerTextEditorCommand(
-        `chineseTranslation.${name}`,
-        (textEditor, edit) => translate(name, textEditor, edit)
-      )
-    );
-  });
+	[
+		'hongKongToSimplified',
+		'simplifiedToHongKong',
+		'simplifiedToTraditional',
+		'simplifiedToTaiwan',
+		'simplifiedToTaiwanWithPhrases',
+		'traditionalToHongKong',
+		'traditionalToSimplified',
+		'traditionalToTaiwan',
+		'taiwanToSimplified',
+		'taiwanToSimplifiedWithPhrases'
+	].map(name => {
+		context.subscriptions.push(
+			vscode.commands.registerCommand(
+				`chineseTranslation.${name}`,
+				() => (translate(name))
+			)
+		);
+	})
 }
 
-function getSelectionOrCurrentLine(textEditor) {
-  let selection = textEditor.selection;
-
-  const
-    start = selection.start,
-    end = selection.end;
-
-  if (start.line === end.line && start.character === end.character) {
-    selection = new vscode.Range(start.line, 0, start.line, Infinity);
-
-    const line = textEditor.document.getText(selection);
-
-    selection = new vscode.Range(start.line, 0, start.line, line.length);
-  }
-
-  return selection;
-}
-
-function translate(name, textEditor, edit) {
-  const selection = getSelectionOrCurrentLine(textEditor);
-
-  return opencc[name](textEditor.document.getText(selection)).then(translated =>
-    textEditor.edit(edit =>
-      edit.replace(selection, translated)
-    )
-  ).catch(err => vscode.window.showErrorMessage(`Failed to translate Chinese text due to ${err.message}`));
+function translate(name) {
+	try {
+		let editor = vscode.window.activeTextEditor,
+			selection = editor.selection,
+			text = editor.document.getText(selection),
+			result = opencc[name](text),
+			snippet = new vscode.SnippetString(result);
+		editor.insertSnippet(snippet);
+	} catch (err) {
+		vscode.window.showErrorMessage(`Failed to translate Chinese text due to ${err.message}`)
+	}
 }
 
 exports.activate = activate;
 
-function deactivate() {
-}
+function deactivate() { }
 
-exports.deactivate = deactivate;
+module.exports = {
+	activate,
+	deactivate
+}
